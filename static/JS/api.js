259f7a5d2f1fa091ecec55a2059a6ae288351d26
DingoFreestyle <104494672+DingoFreestyle@users.noreply.github.com>
2022-07-01 23:15:29 +0900

    FIX - 백엔드 연동 오류 수정

--- static/JS/api.js
+++ static/JS/api.js
@@ -1,12 +1,14 @@
 const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5000"
+const frontend_base_url = "http://127.0.0.1:5500"
 
+
+// 회원가입
 async function handleSignin() {
     const signupData = {
-        username : document.getElementById("floatingInput").value,
-        password : document.getElementById('floatingPassword').value,
-        email : document.getElementById('floatingEmail').value,
-        fullname : document.getElementById('floatingFullname').value,
+        username: document.getElementById("floatingInput").value,
+        password: document.getElementById('floatingPassword').value,
+        email: document.getElementById('floatingEmail').value,
+        fullname: document.getElementById('floatingFullname').value,
         // date : document.getElementById('floatingDate').value,
     }
 
@@ -22,8 +24,52 @@ async function handleSignin() {
     response_json = await response.json()
 
     if (response.status == 200) {
-        window.location.replace('${frontend_base_url}/templates/sign_in.html');
+        alert("회원가입 완료")
+        window.location.replace(`${frontend_base_url}/templates/sign_in.html`);
     } else {
         alert(response.status)
     }
+}
+
+// 로그인
+async function handleLogin() {
+    // console.log("handle login")
+
+    const loginData = {
+        username: document.getElementById("floatingInput").value,
+        password: document.getElementById('floatingPassword').value
+    }
+
+    const response = await fetch(`${backend_base_url}/user/api/token/`, {
+        headers: {
+            Accept: "application/json",
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(loginData)
+    }
+    )
+
+    response_json = await response.json()
+    console.log(response_json.access)
+
+
+    if (response.status == 200) {
+        localStorage.setItem("access", response_json.access)
+        localStorage.setItem("refresh", response_json.refresh)
+
+
+        const base64Url = response_json.access.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        localStorage.setItem("payload", jsonPayload);
+        alert("${{ user.username }}님의 접속을 환영합니다")
+        window.location.replace(`${frontend_base_url}/templates/main.html`);
+    } else {
+        alert(response.status)
+    }
+
 }
