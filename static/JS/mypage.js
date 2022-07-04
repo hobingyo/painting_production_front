d15469b43d18b4e144d18157b21f4e74da42e83f
DingoFreestyle <104494672+DingoFreestyle@users.noreply.github.com>
2022-07-04 15:49:17 +0900

    FIX - 마이페이지 프론트 정렬기능 수정 및 필터 이미지 추가

--- static/JS/mypage.js
+++ static/JS/mypage.js
@@ -26,20 +26,21 @@ window.onload = async function getMyData() {
             let exposure_start_date = detail['article_set'][i]['exposure_start_date']
             let article_id = detail['article_set'][i]['id']
             let temp_html = `
-            <div class="row justify-content-md-center">
-            <div class="col-md-auto" >
-            <div class="my_article" id="mypage-box">
-            <div class="article_wrap">
-                <div><b id="title"><a href="${frontend_base_url}/templates/article_detail.html?id=${article_id}"> ${title}</b><span> | ${exposure_start_date}</span></div>
-                <!--얘는 삭제예정-->
-                <div class="image_frame">
-                    <img src="../static${image}" alt="" />
+            <div class="col-4">
+                <div class="my_article" id="mypage-box">
+                    <div class="article_wrap">
+                        <div> ${exposure_start_date}</span></div>
+                        <!--얘는 삭제예정-->
+                        <div class="image_frame">
+                            <img src="../static${image}" alt="" />
+                        </div>
+                        <div class="article_title">
+                        <b id="title" style="font-size=300px;"><a href="${frontend_base_url}/templates/article_detail.html?id=${article_id}"> ${title}</b><span>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-        </div>
-        </div>`
-            $('#mypage-box').append(temp_html)
+            </div>`
+            $('#mypage-box').prepend(temp_html)
             console.log(detail)
         }
     })
